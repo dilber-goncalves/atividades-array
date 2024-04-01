f3137737d621e1efc474f0de91f4ff9a14cc70d6
Dilber Goncalves <dilberkeybd@gmail.com>
2024-04-01 16:46:42 -0300

    01-04

---
+++ apenas-pares.js
@@ -0,0 +1,9 @@
+const original = [1, 4, 12, 21, 53, 88, 112];
+let pares = [];
+
+for (let numero of original){
+    if (numero % 2 === 0){
+        pares.push(numero); // coloca o numero dentro do array 
+    }
+}
+console.log(pares);
