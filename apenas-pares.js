e76b2bcbf6ce32470438bbdc96f93e2d12d34616
Dilber Goncalves <dilberkeybd@gmail.com>
2024-04-02 17:16:55 -0300

    02-04

--- apenas-pares.js
+++ apenas-pares.js
@@ -1,9 +1,19 @@
-const original = [1, 4, 12, 21, 53, 88, 112];
+const numeros = [1, 3, 4, 12, 7, 21, 8, 5, 10, 53, 88, 112];
 let pares = [];
 
-for (let numero of original){
+for (let numero of numeros){
     if (numero % 2 === 0){
         pares.push(numero); // coloca o numero dentro do array 
     }
 }
 console.log(pares);
+
+//soma dos pares
+
+let somaDosPares = 0;
+for (let numero1 of numeros){
+    if (numero1 % 2 === 0){
+        somaDosPares += numero1;
+    }
+}
+console.log(somaDosPares)
